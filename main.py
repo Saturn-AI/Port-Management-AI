ab9e0df0f210af8dd108ebc109f8c8c7cf7390ea
Hossain Al Mahdi <hame.mahadi@gmail.com>
2024-04-23 17:09:04 +0600

    code update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import folium
 import streamlit as st
 from streamlit_folium import st_folium
 from streamlit_player import st_player
+import folium
 from datetime import datetime
 import pandas as pd
 import time
